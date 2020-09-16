fd0bdfac34d34813ed204cf09f0e6fd4c670fc2d
Alexey Zabelin <hello@alexeyzabelin.com>
2020-09-15 22:59:18 -0500

    Add routes

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,16 @@ async fn main() {
         .and(with_db(db_pool.clone()))
         .and_then(handler::health_handler);
 
+    let todo = warp::path("todo");
+    
+    let todo_routes = todo
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(with_db(db_pool.clone()))
+        .and_then(handler::create_todo_handler);
+
     let routes = health_route
+        .or(todo_routes)
         .with(warp::cors().allow_any_origin())
         .recover(error::handle_rejection);
 
